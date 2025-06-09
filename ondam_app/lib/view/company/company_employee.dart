32017958130593c730315db2f428333f20689621
runpon <ktm0499@gmail.com>
2025-06-09 16:28:47 +0900

    main chart 추가

--- ondam_app/lib/view/company/company_employee.dart
+++ ondam_app/lib/view/company/company_employee.dart
@@ -8,13 +8,110 @@ class CompanyEmployee extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    controller.fetchEmployee();
     return Scaffold(
       backgroundColor: Color(0xFFF6F7FB),
       body: Row(
         children: [
           // CompanySideMenu(),
-        ]
-      )
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(40, 40, 20, 20),
+                      child: Text('직원관리', style: TextStyle(fontSize: 40)),
+                    ),
+                  ],
+                ),
+                Obx(
+                  () => Expanded(
+                    child: ListView.builder(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 40,
+                        vertical: 20,
+                      ),
+                      itemCount: controller.employeeList.length,
+                      itemBuilder: (context, index) {
+                        final employee = controller.employeeList[index];
+                        return Center(
+                          child: Container(
+                            width: 600,
+                            margin: EdgeInsets.only(bottom: 16),
+                            padding: EdgeInsets.all(16),
+                            decoration: BoxDecoration(
+                              color: Colors.white, // 배경 색
+                              borderRadius: BorderRadius.circular(
+                                16,
+                              ), // 모서리 둥글게
+                              boxShadow: [
+                                // 그림자 효과
+                                BoxShadow(
+                                  color: Colors.grey.withAlpha(
+                                    (255 * 0.2).round(),
+                                  ),
+                                  blurRadius: 8,
+                                  offset: Offset(0, 4),
+                                ),
+                              ],
+                              border: Border.all(
+                                color: Colors.grey.shade300,
+                              ), // 연한 테두리
+                            ),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                // 이미지
+                                SizedBox(width: 20),
+                                // 이름과 가격
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        employee.managerId,
+                                        style: TextStyle(
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                      SizedBox(height: 4),
+                                      Text(
+                                        employee.companyCode,
+                                        style: TextStyle(
+                                          fontSize: 22,
+                                          color: Colors.black54,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                      SizedBox(height: 4),
+                                      Text(
+                                        employee.location,
+                                        style: TextStyle(
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                // 버튼들
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
