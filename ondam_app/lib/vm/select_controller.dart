f2436a8c342e52a661ed699382ef32e62aebf90f
runpon <ktm0499@gmail.com>
2025-06-10 15:24:46 +0900

    대리점 메뉴 추가 삭제

--- ondam_app/lib/vm/select_controller.dart
+++ ondam_app/lib/vm/select_controller.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 import 'package:ondam_app/vm/purchase_controller.dart';
 
 class SelectController extends PurchaseController {
@@ -22,5 +25,24 @@ class SelectController extends PurchaseController {
 
   void clearSelectedDrawerItem() {
     selectedDrawerItem.value = null;
+  }
+
+// -- 대리점 메뉴 선택
+  Future<dynamic> insertSelect(String companyId, String menuCode) async {
+    try{
+    final res = await http.get(Uri.parse('$baseUrl/taemin/insert/select?menuCode=$menuCode&companyId=$companyId'));
+    final result = json.decode(utf8.decode(res.bodyBytes))['result'];
+    return result;
+    } catch(e){
+      //
+    }
+  }
+
+  // 대리점 메뉴 삭제
+  Future<String> deleteSelect(String companyId, String menuCode) async {
+    final uri = Uri.parse('$baseUrl/taemin/delete/select?menuCode=$menuCode&companyId=$companyId');
+    final res = await http.get(uri);
+    final result = json.decode(utf8.decode(res.bodyBytes))['result'];
+    return result;
   }
 }
